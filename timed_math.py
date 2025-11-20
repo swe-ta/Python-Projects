63a310f6e3d947c647651b8434a5cef0d526df2b
sweta <ghoshswetacob04@gmail.com>
2025-11-21 01:15:21 +0530

    Modified

--- timed_math.py
+++ timed_math.py
@@ -1,2 +1,38 @@
-for i in range(10):
-    print(i)
+import random
+import time
+
+operators = ["+","-","*"]
+min_operand = 3
+max_operand = 12
+total_problems = 10
+
+def generate_problem():
+    left = random.randint(min_operand,max_operand)
+    right = random.randint(min_operand,max_operand)
+    operator = random.choice(operators)
+
+    expr = str(left) + "" + operator + "" + str(right)
+    answer = eval(expr)
+    return expr,answer
+
+
+wrong = 0
+input("Press Enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
+
+for i in range(total_problems):
+    expr,answer = generate_problem()
+    while True:
+      guess = input("Problem #" + str(i+1) + ":" +expr+ "=")
+      if guess == str(answer):
+          break
+
+      wrong += 1
+
+end_time = time.time()
+total_time = round(end_time+start_time,2)
+print("You finised in ",total_time,"seconds!")
+
